7f797ee09a6bcb5524d221602edf17d4421359a7
kasscom12 <kassahutadele2021@gmail.com>
2022-09-11 13:45:59 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,13 @@
 /**
  * main - print the string in the put function
  *
- * Description: 'using the main function'
- * this program prints "with proper grammar, but the outcome is a piece of art
- * return: 0
- */
+ * Description: using the main function
+ * this program prints "with proper grammer, but the outcome is piece of art,
+ * Return: 0
+ * */
 int main(void)
 {
-	printf("with proper grammar,but the outcome is a piece of art,\n");
+	printf("with proper grammer, but the outcome is piece of art,\n);
 	return (0);
 }
+
